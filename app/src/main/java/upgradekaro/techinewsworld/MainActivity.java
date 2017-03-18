c7729085f442968059f7486c0edd53c785f5d45a
ajay <ajay.kumar@credencys.com>
2017-03-18 18:38:04 +0530

    "18th march"

--- app/src/main/java/upgradekaro/techinewsworld/MainActivity.java
+++ app/src/main/java/upgradekaro/techinewsworld/MainActivity.java
@@ -1,12 +1,8 @@
 package upgradekaro.techinewsworld;
 
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,65 +11,33 @@ import upgradekaro.techinewsworld.fragments.GeneralFragment;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
+    DrawerLayout sidemenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseDatabase.getInstance();
-        Location();
+        init();
         FragmentProcess();
+    }
+
+    private void init() {
+        sidemenu= (DrawerLayout) findViewById(R.id.drawer);
     }
 
     public void FragmentProcess() {
         GeneralFragment fragment = new GeneralFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.activitymain_container, fragment);
-        fragmentTransaction.addToBackStack(null);
+     //   fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
 
     @Override
     public void onBackPressed() {
-        finish();
+      //  finish();
         super.onBackPressed();
-    }
-
-    public void Location() {
-        LocationManager locationManager = (LocationManager) this.getSystemService(MainActivity.this.LOCATION_SERVICE);
-
-// Define a listener that responds to location updates
-        LocationListener locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                // Called when a new location is found by the network location provider.
-                makeUseOfNewLocation(location);
-            }
-
-            private void makeUseOfNewLocation(Location location) {
-            }
-
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-
-            public void onProviderEnabled(String provider) {
-            }
-
-            public void onProviderDisabled(String provider) {
-            }
-        };
-
-// Register the listener with the Location Manager to receive location updates
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
 }
